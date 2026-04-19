99251bba5b0eefd835f2af6150027f01e1cf1354
Macoumba Guédel Mbodj <macoumbaguedelmbodj@gmail.com>
2026-04-19 16:37:06 +0200

    Add script to disable a user in Microsoft Entra ID

--- Leaver/disable-user.ps1
+++ Leaver/disable-user.ps1
@@ -1,1 +1,16 @@
+# Connexion à Microsoft Entra ID
+Connect-MgGraph
+
+# Identifiant de l'utilisateur à désactiver (Leaver)
+$UPN = "jean.dupont@labiam2025.onmicrosoft.com"
+
+# Récupération de l'utilisateur
+$User = Get-MgUser -UserId $UPN
+
+# Désactivation du compte
+Update-MgUser `
+ -UserId $User.Id `
+ -AccountEnabled $false
+
+Write-Host "Compte utilisateur désactivé avec succès"
 
